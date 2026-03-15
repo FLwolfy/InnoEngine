49ad3a58a7892686b8d37ef6aa981fe1590bf4bf
Hsuan-Kai Liao <hl475@duke.edu>
2026-03-16 01:00:31 +0800

    Change build directory name for cimgui build

--- build/Inno.Build.Cimgui/CimguiBuildConstants.cs
+++ build/Inno.Build.Cimgui/CimguiBuildConstants.cs
@@ -4,6 +4,6 @@ public static class CimguiBuildConstants
 {
     public const string OUTPUT_PRODUCT_DIR_NAME = "cimgui";
     public const string CIMGUI_DIR_NAME = "cimgui";
-    public const string BUILD_DIR_NAME = ".build";
+    public const string BUILD_DIR_NAME = "bld";
     public const string CMAKE_LISTS_FILE = "CMakeLists.txt";
 }
